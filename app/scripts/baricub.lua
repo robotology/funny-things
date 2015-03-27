3d61d22002906297ca3eb7af1eb374b4c7b6690d
Ugo Pattacini <ugo.pattacini@iit.it>
2015-03-27 15:30:08 +0100

    lua modified

--- app/scripts/baricub.lua
+++ app/scripts/baricub.lua
@@ -3,19 +3,48 @@
 require("yarp")
 yarp.Network()
 
-port=yarp.Port()
-port:open("/lua")
+port_events=yarp.Port()
+port_speak=yarp.Port()
+port_speak_opt=yarp.Port()
+port_events:open("/iCubBarMenu/manager/events:i")
+port_speak:open("/iCubBarMenu/manager/speak:o")
+port_speak_opt:open("/iCubBarMenu/manager/speak:rpc")
 
-bottle=yarp.Bottle();
-port:read(bottle);
-drink=bottle:get(1):toString();
-print("drink received: " .. drink);
-port:close()
+yarp.NetworkBase_connect("/iCubBarMenu/events:o","/iCubBarMenu/manager/events:i")
+yarp.NetworkBase_connect("/iCubBarMenu/manager/speak:o","/iSpeak")
+yarp.NetworkBase_connect("/iCubBarMenu/manager/speak:rpc","/iSpeak/rpc")
+
+event=yarp.Bottle()
+port_events:read(event)
+drink=event:tail():toString()
+print("drink received: " .. drink)
+
+speech=yarp.Bottle()
+speech:addString("ah! bella scelta! allora adesso ti preparo un bel")
+port_speak:write(speech)
+
+opt=yarp.Bottle()
+rep=yarp.Bottle()
+opt:addString("set")
+opt:addString("opt")
+opt:addString("icub_eng")
+port_speak_opt:write(opt,rep)
+
+speech:clear()
+speech:addString(drink)
+port_speak:write(speech)
+
+opt:clear()
+opt:addString("set")
+opt:addString("opt")
+opt:addString("icub_ita")
+port_speak_opt:write(opt,rep)
+
+--print("./baricub.sh run_all")
+--os.execute("./baricub.sh run_all")
+
+port_events:close()
+port_speak:close()
+port_speak_opt:close()
+
 yarp.Network_fini()
-
-command="baricub speak ";
-print(command .. drink);
-os.execute(command .. drink);
-
-print("baricub run_all");
-os.execute("baricub run_all");
