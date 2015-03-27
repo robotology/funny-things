3b2e710a4385b597429e8780b49143bda3162f6a
Ugo Pattacini <ugo.pattacini@iit.it>
2015-03-27 17:30:00 +0100

    update

--- app/scripts/baricub.sh
+++ app/scripts/baricub.sh
@@ -38,23 +38,25 @@ reach_glass() {
     speak "prendiamo il nostro bel bicchierino da cocktail"
     echo "ctpq time 1.5 off 0 pos (4.0 48.0 5.0 79.0 -45.0 -20.0 -19.0 50.0 0.0 10.0 10.0 10.0 10.0 10.0 10.0 10.0)" | yarp rpc /ctpservice/right_arm/rpc
     sleep 0.5
-    speak "e' viola, frivolo come piace a me"
     echo "ctpq time 2.7 off 0 pos (-9.0 0.0 17.0)" | yarp rpc /ctpservice/torso/rpc
     echo "ctpq time 1.0 off 0 pos (10.0 50.0 15.0 63.0 -12.0 -36.0 -15.0 38.0 10.0 10.0 10.0 10.0 10.0 10.0 10.0 10.0)" | yarp rpc /ctpservice/left_arm/rpc
     echo "ctpq time 1.3 off 0 pos (-40.0 17.0 -18.0 33.0 18.0 0.0 -20.0 38.0 90.0 10.0 10.0 10.0 10.0 10.0 10.0 10.0)" | yarp rpc /ctpservice/left_arm/rpc
     echo "ctpq time 1.0 off 0 pos (-40.0 17.0 -18.0 33.0 18.0 0.0 -20.0 38.0 90.0 14.0 66.0 35.0 77.0 20.0 86.0 10.0)" | yarp rpc /ctpservice/left_arm/rpc
     echo "ctpq time 1.0 off 0 pos (-40.0 17.0 -18.0 33.0 18.0 0.0 -20.0 38.0 90.0 14.0 66.0 35.0 77.0 20.0 86.0 120.0)" | yarp rpc /ctpservice/left_arm/rpc
-    sleep 7.0
+    sleep 3.0
+    speak "e' viola, frivolo come piace a me"
+    sleep 4.0
 }
 
 reach_sugar() {
     speak "dov'e lo zucchero?"
     look_at -0.35 0.25 -0.1
-    speak "eccolo!"
     echo "ctpq time 2.0 off 0 pos (-12.0 26.0 1.0 71.0 -28.0 0.0 -18.0 50.0 90.0 10.0 10.0 10.0 10.0 10.0 10.0 10.0)" | yarp rpc /ctpservice/right_arm/rpc
     echo "ctpq time 1.0 off 0 pos (-12.0 26.0 1.0 71.0 -28.0 0.0 -18.0 50.0 90.0 20.0 30.0 50.0 85.0 45.0 85.0 10.0)" | yarp rpc /ctpservice/right_arm/rpc
     echo "ctpq time 1.0 off 0 pos (-12.0 26.0 1.0 71.0 -28.0 0.0 -18.0 50.0 90.0 20.0 30.0 50.0 85.0 45.0 85.0 120.0)" | yarp rpc /ctpservice/right_arm/rpc
-    sleep 6.0
+    sleep 2.0
+    speak "eccolo!"
+    sleep 4.0
 }
 
 reach_in_hands() {
@@ -99,27 +101,27 @@ push_liquid() {
     echo "ctpq time 2.0 off 0 pos (-31.0 0.0 0.0)" | yarp rpc /ctpservice/torso/rpc
     echo "ctpq time 2.0 off 0 pos (10.0 60.0 29.0 86.0 -45.0 0.0 -20.0 38.0 90.0 14.0 66.0 35.0 77.0 20.0 86.0 120.0)" | yarp rpc /ctpservice/left_arm/rpc
     sleep 2.5
-    echo "-0.135 -0.360 -0.005 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
+    echo "-0.135 -0.360 -0.015 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
     sleep 4.0
-    echo "-0.120 -0.300 -0.015 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
+    echo "-0.120 -0.300 -0.025 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
     sleep 1.0
-    echo "-0.090 -0.300 -0.010 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
+    echo "-0.090 -0.300 -0.020 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
     sleep 1.5
 }
 
 pour_syrup() {
     look_at -0.45 -0.250 0.0
     speak "1 cl di succo di laim, fresco filtrato"
-    echo "-0.220 -0.250 0.036 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
+    echo "-0.220 -0.250 0.030 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
     sleep 2.0
-    echo "-0.310 -0.250 0.036 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
+    echo "-0.310 -0.250 0.030 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
     sleep 3.0
-    echo "-0.220 -0.250 0.036 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
+    echo "-0.220 -0.250 0.030 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
     sleep 3.0
     speak "un altro po', perche no?"
-    echo "-0.310 -0.250 0.036 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
+    echo "-0.310 -0.250 0.030 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
     sleep 3.0
-    echo "-0.220 -0.250 0.036 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
+    echo "-0.220 -0.250 0.030 -0.155917 0.702537 -0.694357 2.598576" | yarp write ... /armCtrl/left_arm/xd:i
     sleep 2.0
 }
 
