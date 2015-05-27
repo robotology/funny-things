797e48520ee21a65e83a404892840aed9be3d19f
Alessandro Roncone <alecive87@gmail.com>
2015-05-27 11:14:25 +0200

    Updates to the blinking script

--- app/scripts/blinking-experiments.sh
+++ app/scripts/blinking-experiments.sh
@@ -66,12 +66,36 @@ wait_till_quiet() {
 }
 
 speak_hri() {
-    blink
+    # blink
     speak "$1"
     sleep 0.15
     wait_till_quiet
-    blink    
+    # blink    
 }
+
+#do a blink only at the beginning (onset)
+speak_hri_b() {
+    # blink
+    speak "$1"
+    sleep 0.15
+    wait_till_quiet
+}
+
+#do not blink at all at onset/offset
+speak_hri_n() {
+    speak "$1"
+    sleep 0.15
+    wait_till_quiet
+}
+
+#do a blink only at the end (offset)
+speak_hri_e() {
+    speak "$1"
+    sleep 0.15
+    wait_till_quiet
+    # blink
+}
+
 
 #######################################################################################
 # FUNCTION TO CALL
@@ -84,18 +108,18 @@ run() {
     sleep 8.0
     speak_hri "Sure, go ahead. What would you like to know?"
     sleep 4.5
-    speak_hri "Hmmm, let's see."
-    speak_hri "So, typically, one of the humans working with me, stands on the other side of a table located between the two of us."
-    speak_hri "Then, I am shown different objects on the table, and the human asks me: 'Where is the octopus?'"
-    speak_hri "I then point to one of the objects, that resembles an octopus and say: 'I think this is the octopus.'."
+    speak_hri_b "Hmmm, let's see."
+    speak_hri_n "So, typically, one of the humans working with me, stands on the other side of a table located between the two of us."
+    speak_hri_n "Then, I am shown different objects on the table, and the human asks me: 'Where is the octopus?'"
+    speak_hri_e "I then point to one of the objects, that resembles an octopus and say: 'I think this is the octopus.'."
     sleep 4.0
     speak_hri "Well, it is more like recognizing different shapes and changes in my surroundings."
     sleep 4.0
-    speak_hri "The humans working with me, usually tell them to me while pointing at the objects."
-    speak_hri "They do this quite often and repetitively."
+    speak_hri_b "The humans working with me, usually tell them to me while pointing at the objects."
+    speak_hri_e "They do this quite often and repetitively."
     sleep 7.0
-    speak_hri "Not really? We also shuffle the objects on the table around, so I can restart looking for the octopus."
-    speak_hri "Sometimes, if the humans feel very adventurous, I am also shown new objects, and then I can start looking for them on the table."
+    speak_hri_b "Not really? We also shuffle the objects on the table around, so I can restart looking for the octopus."
+    speak_hri_e "Sometimes, if the humans feel very adventurous, I am also shown new objects, and then I can start looking for them on the table."
     sleep 5.0
     speak_hri "No problem. my pleasure."
     sleep 1.6
