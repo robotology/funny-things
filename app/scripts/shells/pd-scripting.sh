b4298d8c24ab33f310b8e6c8bb3d5ed84635b85d
icub <icub@icub23.icub.local>
2016-09-27 15:43:37 +0200

    code for parametric design presentation

--- app/scripts/shells/pd-scripting.sh
+++ app/scripts/shells/pd-scripting.sh
@@ -84,12 +84,12 @@ start_breathers() {
 
 go_home_helperL() {
     # This is with the arms close to the legs
-     echo "ctpq time $1 off 0 pos (-21.7695	17.9352	0.203248	28.0262	0	0	-0.0439454	15.3864	29.8334	1.95557	11.7389	0.351563	7.43227	2.46644	1.06018	2.38404	)" | yarp rpc /ctpservice/left_arm/rpc
+     echo "ctpq time $1 off 0 pos (-24.7695	17.9352	0.203248	29.11	18	0	-0.0439454	15.3864	29.8334	1.95557	11.7389	0.351563	7.43227	2.46644	1.06018	2.38404	)" | yarp rpc /ctpservice/left_arm/rpc
 }
 
 go_home_helperR() {
     # This is with the arms close to the legs
-     echo "ctpq time $1 off 0 pos (-21.7 24.1535 23.5987	28.1361	-22.7912	0	-4.42201	18.1385	28.9106	0	5.46571	0.373536	1.66993	0.379029	0	-0.422975	)" | yarp rpc /ctpservice/right_arm/rpc
+     echo "ctpq time $1 off 0 pos (-21.7 24.1535 23.5987	28.1361	-6.7912	0	-4.42201	18.1385	28.9106	0	5.46571	0.373536	1.66993	0.379029	0	-0.422975	)" | yarp rpc /ctpservice/right_arm/rpc
 }
 
 go_home_helper() {
@@ -312,35 +312,65 @@ talk_mic() {
     #breathers "start"
 }
 
+look_at_presentatore(){
+gaze "idle"
+gaze "look 30.0 0.0 5.0"
+}
+
+look_at_schermo(){
+gaze "idle"
+gaze "look 45.0 10.0 3.0"
+}
+
+look_at_pubblico(){
+gaze "idle"
+gaze "look -30.0 -10.0 5.0"
+}
+
 look_presentatore(){
-gaze "look-around 15.0 0.0 5.0"
+gaze "look-around 30.0 0.0 5.0"
 }
 
 look_schermo(){
-gaze "look-around 30.0 10.0 3.0"
+gaze "look-around 45.0 10.0 3.0"
 }
 
 look_pubblico(){
-gaze "look-around -30.0 0.0 5.0"
+gaze "look-around -30.0 -10.0 5.0"
 }
 
 look_tavolo(){
 gaze "look 15.0 -10.0 -5.0"
 }
+
+torso_home(){
+echo "ctpq time 2 off 0 pos (0 0 0 )" | yarp rpc /ctpservice/torso/rpc
+}
+
+torso_pubblico(){
+echo "ctpq time 2 off 0 pos (-20 0 0 )" | yarp rpc /ctpservice/torso/rpc
+}
+
+torso_presentatore(){
+echo "ctpq time 2 off 0 pos (10 0 0 )" | yarp rpc /ctpservice/torso/rpc
+}
+
 #######################################################################################
 # SEQUENCE FUNCTIONS
 #######################################################################################
 sequence_00() {
-    gaze "look-around 15.0 0.0 3.0"
+    gaze "look-around 0.0 0.0 3.0"
+    go_home_helperL 3.0
+    go_home_helperR 3.0
     smile
 }
 
 sequence_01() {
 #    gaze "idle"
-    gaze "look 30.0 5.0 3.0" 
+    look_presentatore 
     speak "Ciao Fabrizio."
     sleep 1.0
-    gaze "look-around -30.0 0.0 5.0"
+    look_pubblico
     speak "Sono contento di essere qui con voi oggi."
     sleep 1.0 
 #   gaze "idle"
@@ -355,38 +385,45 @@ sequence_02() {
 #stringe la mano
 echo "ctpq time 1.5 off 0 pos (-36.8 24.3 23.6 55.558 -22.791 0.0 -4.5	54.9 29.8 13.7824 42.16	0.0	2.0 0.0	0.0	0.0 )" | yarp rpc /ctpservice/right_arm/rpc
 #sleep 10.0
+echo "ctpq time 2 off 0 pos (10 0 5 )" | yarp rpc /ctpservice/torso/rpc
     gaze "idle"
-    gaze "look 15.0 0.0 5.0"
-
+    look_presentatore
+echo "set intm mode 3 comp" | yarp rpc /icub/right_arm/rpc:i
+wait_till_quiet
+smile
 }
 
 
 sequence_03() {
 #guarda lo schermo
-gaze "look 30.0 10.0 3.0"
+look_schermo
 sleep 1.0
-speak "Non mi era mai successo di vedere cosa sentono i miei sensori!"
+speak "Nonmi era mai successo di vedere cosa sentono i miei sensori!"
 wait_till_quiet
 surprised
 sleep 1.0
-gaze "look 15.0 0.0 5.0"
+look_presentatore
 smile
 sleep 1.0
 speak "Ee molto interessante"
 sleep 1.0
-gaze "look-around -30.0 0.0 5.0"
+look_pubblico
 speak "ma mi sembra di capire"
-speak "che anche da molto lontano qualcuno potrebbe vedere cosa ri levano i miei sensori."
+speak "che anche da molto lontano qualcuno potrebbe vedere quello che ri levano i miei sensori."
 sleep 6.0
-gaze "look-around 15.0 0.0 3.0"
+look_presentatore
 wait_till_quiet
 smile
 }
 
 sequence_04(){
 #rilascia la mano
+echo "set intm mode 3 stiff" | yarp rpc /icub/right_arm/rpc:i
 go_home_helperR 2.0
-gaze "look-around -30.0 0.0 5.0"
+torso_home
+look_pubblico
+wait_till_quiet
+smile
 }
 
 
@@ -399,14 +436,15 @@ look_schermo
 sequence_06(){
 # guarda in giro
 look_presentatore
-sleep 2
-speak "Certamente!"
+sleep 0.5
+speak "Certamente!!!!"
+wait_till_quiet
 smile
 }
 
 
 sequence_07(){
-speak "Sicuro!"
+speak "Sicuro!!!!"
 sleep 1.0
 speak "Prova a guardare sul tavolo qui di fianco"
 look_tavolo
@@ -418,26 +456,68 @@ go_home_helperR 2.0
 speak "Troverai un aiPad, e un mio braccio nonancora completamente assemblato."
 sleep 3.0
 speak "Credo che riuscirai facilmente a capire qualcosa di piuu"
+wait_till_quiet
+smile
 
 }
 
 sequence_08() {
-speak "Fabrizio, mi gira un po' la testa!"
-sleep 1.0
+gaze "idle"
+speak "Fabriiizioooo!!"
+sad
+sleep 0.3
+speak " Mi giira un po' la testa!"
+gaze "look 10.0 20.0 5.0"
+echo "ctpq time 1.5 off 0 pos (0 -10 15 )" | yarp rpc /ctpservice/torso/rpc
+sleep 0.3
+sad
+gaze "look -10.0 20.0 5.0"
+sleep 0.3
+gaze "look 0.0 20.0 5.0"
+echo "ctpq time 1.5 off 0 pos (0 10 45 )" | yarp rpc /ctpservice/torso/rpc
+sleep 0.3
+gaze "look 20.0 -70.0 -10.0"
+#echo "ctpq time 1.5 off 3 pos (0 -10 15 )" | yarp rpc /ctpservice/torso/rpc
+}
+
+sequence_09()
+{
+    torso_home
+    sleep 1
+    look_presentatore
+    smile
 }
 
 sequence_10(){ 
-smile
-sleep 1.0
+look_schermo
 speak "Grazie ragazzi!"
 sleep 1.0
 speak "Adesso mi sento veramente meglio!"
+look_pubblico
 fonzie
-
+wait_till_quiet
+smile
 }
 
 
+sequence_11(){ 
+look_presentatore
+torso_home
+speak "Grazie, anche per me Ee stato divertente!"
+wait_till_quiet
+look_at_pubblico
+sleep 0.5
+smile
 
+speak "Dopo di noi vedrete molte altre cose interessanti!"
+torso_pubblico
+wait_till_quiet
+speak "Auguro a tutti una buona giornata qui con noi all'Istituto Italiano di Tecnologia."
+hello_left
+wait_till_quiet
+smile
+torso_home
+}
 
 #######################################################################################
 # "MAIN" FUNCTION:                                                                    #
