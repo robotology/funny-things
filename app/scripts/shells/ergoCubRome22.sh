a50ac0339e2b6875c7a6f65f353a09f67c5c7b24
Simone Girardi <simone.girardi@iit.it>
2022-11-29 13:14:43 +0100

    ergoCub: update FunyThings including Rome demos

--- app/scripts/shells/ergoCubRome22.sh
+++ app/scripts/shells/ergoCubRome22.sh
@@ -78,6 +78,18 @@ look_right_down() {
 }
 
 
+look_around_loop() {
+n=0
+while [ True ]
+do
+    look_around
+    echo $n
+    n=$((n+1))
+    sleep 11
+done
+}
+
+
 look_around() {
     look_left_down
     look_right_up
@@ -88,7 +100,7 @@ look_around() {
 
 
 torso_low(){
-    echo "ctpq time 2 off 0 pos (-4.32862 1.23047 33.4864)" | yarp rpc /ctpservice/torso/rpc
+    echo "ctpq time 2 off 1 pos (1.23047 33.4864)" | yarp rpc /ctpservice/torso/rpc
 }
 
 torso_high(){
@@ -109,12 +121,22 @@ set_up_left_arm(){
 }
 
 open_left_hand() {
-    echo "ctpq time 2.0 off 7 pos (20.0 35.0 17.0 1.0 1.0 1.0 1.0 1.0 0.0)" | yarp rpc /ctpservice/left_arm/rpc
+    echo "ctpq time 2.0 off 7 pos (0.0 0.0 0.0 0.0)" | yarp rpc /ctpservice/left_arm/rpc
 }
 
 
 open_right_hand() {
-    echo "ctpq time 2.0 off 7 pos (20.0 30.0 0.0 1.0 1.0 1.0 1.0 1.0 0.0)" | yarp rpc /ctpservice/right_arm/rpc
+    echo "ctpq time 2.0 off 7 pos (0.0 0.0 0.0 0.0)" | yarp rpc /ctpservice/right_arm/rpc
+}
+
+
+close_left_hand() {
+    echo "ctpq time 2.0 off 7 pos (120.0 120.0 120.0 120.0)" | yarp rpc /ctpservice/left_arm/rpc
+}
+
+
+close_right_hand() {
+    echo "ctpq time 2.0 off 7 pos (120.0 120.0 120.0 120.0)" | yarp rpc /ctpservice/right_arm/rpc
 }
 
 set_up_hand() {
@@ -150,6 +172,51 @@ wave()
 }
 
 
+wave_right()
+{
+    echo "ctpq time 2.5 off 0 pos (0 0 -14 10.0 0 0 0 10.5 10.5 9)" | yarp rpc /ctpservice/left_arm/rpc
+    echo "ctpq time 2.0 off 0 pos (-34 17.0 -27.0 87.0 73 -45 0 10 10 10)" | yarp rpc /ctpservice/right_arm/rpc
+    echo "ctpq time 1.0 off 0 pos (-34 17.0 5 98.0 73 -45 15 20 10 10)"    | yarp rpc /ctpservice/right_arm/rpc
+    echo "ctpq time 1.0 off 0 pos (-23 17.0 -41 91.0 73 -45 -20 10 10 10)" | yarp rpc /ctpservice/right_arm/rpc
+    echo "ctpq time 1.0 off 0 pos (-34 17.0 5 98.0 73 -45 15 20 10 10)"    | yarp rpc /ctpservice/right_arm/rpc
+    echo "ctpq time 1.0 off 0 pos (-23 17.0 -41 91.0 73 -45 -20 10 10 10)" | yarp rpc /ctpservice/right_arm/rpc 
+    home_right_arm
+    sleep 6
+    home_left_arm
+}
+
+
+wave_left()
+{
+    echo "ctpq time 2.5 off 0 pos (0 0 -14 10.0 0 0 0 10.5 10.5 9)" | yarp rpc /ctpservice/right_arm/rpc
+    echo "ctpq time 2.0 off 0 pos (-34 17.0 -27.0 87.0 73 -45 0 10 10 10)" | yarp rpc /ctpservice/left_arm/rpc
+    echo "ctpq time 1.0 off 0 pos (-34 17.0 5 98.0 73 -45 15 20 10 10)"    | yarp rpc /ctpservice/left_arm/rpc
+    echo "ctpq time 1.0 off 0 pos (-23 17.0 -41 91.0 73 -45 -20 10 10 10)" | yarp rpc /ctpservice/left_arm/rpc
+    echo "ctpq time 1.0 off 0 pos (-34 17.0 5 98.0 73 -45 15 20 10 10)"    | yarp rpc /ctpservice/left_arm/rpc
+    echo "ctpq time 1.0 off 0 pos (-23 17.0 -41 91.0 73 -45 -20 10 10 10)" | yarp rpc /ctpservice/left_arm/rpc 
+    home_left_arm
+    sleep 6
+    home_right_arm
+}
+
+
+
+
+look_right_hand()
+{
+    # adjust the left arm
+    echo "ctpq time 2.5 off 0 pos (0 0 -14 10.0 0 0 0 10.5 10.5 9)" | yarp rpc /ctpservice/left_arm/rpc
+    # adjust the right arm
+    echo "ctpq time 4.5 off 0 pos (6 16 -29 89 -88 28 0 0 0 0)" | yarp rpc /ctpservice/right_arm/rpc
+    look_right_down
+    close_right_hand
+    open_right_hand
+    sleep 8
+    home_head
+    home_right_arm
+    home_left_arm
+}
+
 ###################################################################
 #               ACTIONS FOR RUNWAY                                #
 ###################################################################
@@ -170,6 +237,27 @@ home_head() {
     echo "ctpq time 2 off 0 pos (0 0 0 0)" | yarp rpc /ctpservice/head/rpc
 }
 
+home_head() {
+    echo "ctpq time 2 off 0 pos (0 0 0 0)" | yarp rpc /ctpservice/head/rpc
+}
+
+
+loop_test() {
+    n=0
+    while [ True ]
+    do
+        echo "look_around"
+        look_around
+        sleep 11
+        echo "wave"
+        wave_left
+        sleep 4
+        echo "look_right_hand"
+        look_right_hand
+        sleep 6
+        n=$((n+1))
+    done
+}
 
 #######################################################################################
 # "MAIN" FUNCTION:                                                                    #
